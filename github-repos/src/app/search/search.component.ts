ea65d44759e1672cd8bdb153d08e6b485f30bf2e
Priyam Srivastava <79325116+ipriyam26@users.noreply.github.com>
2023-01-11 14:49:32 +0530

    error component

--- github-repos/src/app/search/search.component.ts
+++ github-repos/src/app/search/search.component.ts
@@ -5,6 +5,8 @@ import { faSearch } from '@fortawesome/free-solid-svg-icons';
 import { Repository } from '../repository';
 import { User } from '../user';
 import { SearchEvent } from '../search_event';
+import { catchError, switchMap } from 'rxjs/operators';
+import { of } from 'rxjs';
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
@@ -16,62 +18,49 @@ export class SearchComponent {
   @Output() onSubmit = new EventEmitter<SearchEvent>();
   user: User = {} as User;
   repositories: Repository[] = [];
-  error: any;
+  error: string = '';
 
   constructor(private http: HttpClient) {}
 
   submit() {
     if (!this.username) {
-      this.error = "Please enter a valid username";
+      this.error = 'Please enter a valid username';
       return;
-  }
+    }
 
-  // check for valid input
-  let regExp = new RegExp(/^[a-zA-Z0-9]([a-zA-Z0-9]|[-])*$/);
-  if (!regExp.test(this.username)) {
-      this.error = "Please enter a valid username";
+    // check for valid input
+    let regExp = new RegExp(/^[a-zA-Z0-9]([a-zA-Z0-9]|[-])*$/);
+    if (!regExp.test(this.username)) {
+      this.error = 'Please enter a valid username';
       return;
-  }
+    }
 
-  // check for network connectivity
-  if (!navigator.onLine) {
-      this.error = "You are not connected to the internet";
+    // check for network connectivity
+    if (!navigator.onLine) {
+      this.error = 'You are not connected to the internet';
       return;
-  }
-    this.getUser();
-    this.getRepos();
-  }
-
-  getUser() {
+    }
+    // this.error = null;
     this.http
       .get<User>(`https://gitrepo2.onrender.com/users/${this.username}`)
-      .subscribe(
-        (user: User) => {
+      .pipe(
+        switchMap((user: User) => {
           this.user = user;
-          console.log(this.user)
-        },
-        (error) => {
-          this.error = error;
-        }
-      );
-  }
-
-  getRepos() {
-    this.http
-      .get<Repository[]>(
-        `https://gitrepo2.onrender.com/users/${this.username}/repos`
+          return this.http.get<Repository[]>(
+            `https://gitrepo2.onrender.com/users/${this.username}/repos`
+          );
+        }),
+        catchError((err) => {
+          this.error = 'User not found.';
+          return of();
+        })
       )
-      .subscribe(
-        (repositories: Repository[]) => {
-          this.repositories = repositories;
-          this.onSubmit.emit({
-            user: this.user,
-            repositories: this.repositories,
-          });
-        },
-        (error) => {
-          this.error = error;
-        }
-      );
+      .subscribe((repositories: Repository[]) => {
+        this.repositories = repositories;
+        this.onSubmit.emit({
+          user: this.user,
+          repositories: this.repositories,
+        });
+      });
   }
 }
